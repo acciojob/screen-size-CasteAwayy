ba10aa05b2008f5e0c43ff7f53b11e9ba2632d57
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-03 21:12:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const resizeInfo = document.querySelector('h1');
+const html = `Width : ${window.innerWidth} and Height: ${window.innerHeight}`;
+resizeInfo.textContent = html;
+window.addEventListener('resize' , (e)=>{
+
+    const html = `Width : ${e.target.innerWidth} and Height: ${e.target.innerHeight}`;
+    resizeInfo.textContent = html;
+})
